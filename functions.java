0ea6e48f2a07d995ae209250feaf370fa7418adb
Shreya N <nshreya094@gmail.com>
2024-01-07 23:41:53 +0530

    methods basic program

--- functions.java
+++ functions.java
@@ -13,7 +13,33 @@ public static int calculateSum(int a, int b){
   int sum = a+b;
   return sum;
 }
-public static void main(String args[]){
- 
+public static int product(int a ,int b){
+  int prod = a * b;
+  return prod;
 }
+public static int factorial(int n){
+  int f =1;
+  for(int i =1 ;i<=n; i++){
+    f*=i;
+
+  }
+  return f;
 }
+
+
+  public static int binco(int n, int r) {
+      int fact_n = factorial(n);
+      int fact_r = factorial(r);
+      int fact_nmr = factorial(n - r);
+      int bincoeff = fact_n / (fact_r * fact_nmr);
+      return bincoeff;
+  }
+
+  public static void main(String args[]) {
+      System.out.println(binco(5,5 ));
+  }
+}
+
+
+
+
