70f9b5e2a44990a8bd7ad6ea22f423de14bc6e60
Shreya N <nshreya094@gmail.com>
2024-01-07 22:57:08 +0530

    method basic program

---
+++ functions.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class functions {
+  public static void sum(){
+  Scanner sc = new Scanner(System.in);
+  int a = sc.nextInt();
+  int b = sc.nextInt();
+  int sum = a+b;
+  System.out.println(sum);
+
+  
+}
+public static int calculateSum(int a, int b){
+  int sum = a+b;
+  return sum;
+}
+public static void main(String args[]){
+ 
+}
+}
