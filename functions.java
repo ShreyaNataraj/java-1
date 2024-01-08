985e4552ea25b3a2f8dfe0e9fb2e947c02ec98b8
Shreya N <nshreya094@gmail.com>
2024-01-08 17:54:22 +0530

    functions basic program

--- functions.java
+++ functions.java
@@ -9,14 +9,17 @@ public class functions {
 
   
 }
+//sum of the two numbers
 public static int calculateSum(int a, int b){
   int sum = a+b;
   return sum;
 }
+//product of the two numbers
 public static int product(int a ,int b){
   int prod = a * b;
   return prod;
 }
+//print the factorial of the number
 public static int factorial(int n){
   int f =1;
   for(int i =1 ;i<=n; i++){
@@ -34,11 +37,57 @@ public static int factorial(int n){
       int bincoeff = fact_n / (fact_r * fact_nmr);
       return bincoeff;
   }
+  //print prime numbers in an optimised level
+  public static boolean isprime(int n){
+    for(int i =2; i<=Math.sqrt(n); i++){
+      if(n%i==0){
+        return false;
+      }
+      
+    }
+    return true;
+  }
+  //print all primes in range
+  public static void printprimes(int n){
+    for(int i =2; i<=n; i++){
+      if(isprime(i)){
+        System.out.print(i+ " ");
+      }
+    }
+    System.out.println();
 
+  }
+  //converting binary to decimal numbers
+ public static void bintodec(int binnum){
+  int pow = 0;
+  int decimal = 0;
+  int mynum = binnum;
+  while( binnum!=0){
+    int lastdigit = binnum%10;
+    decimal = decimal + lastdigit*(int)(Math.pow(2,pow));
+    pow++;
+    binnum = binnum/10;
+  }
+  System.out.println("decimal of" + "" + mynum + "is" + " " + decimal);
+ }
+ //converting decimal number to primary numbers
+ public static void dectobin(int decnum){
+  int mynum = decnum;
+  int pow = 0;
+  int binnum = 0;
+  while(decnum>0){
+    int rem = decnum % 2;
+    binnum = binnum + (rem)*(int)(Math.pow(10,pow));
+    pow++;
+    decnum/=2;
+  }
+  System.out.println("binary form of" + mynum + " " + binnum);
+ }
   public static void main(String args[]) {
-      System.out.println(binco(5,5 ));
+     dectobin(23);
   }
 }
+
 
 
 
